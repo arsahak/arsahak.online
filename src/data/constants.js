8c2b028c67d5785a5220fbc0dd2933970d9975ee
AR Sahak <72269399+arsahak@users.noreply.github.com>
2023-12-13 19:30:04 +0600

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/arsahak",
   resume:
-    "https://drive.google.com/file/d/1CLi5UXHeSRMD3FDuol5qIVN62KGkWzbL/view?usp=sharing",
+    "https://drive.google.com/file/d/1WVcJH5BO5up2fGbswAI5ahSIHB6qY3hn/view",
   linkedin: "https://www.linkedin.com/in/arsahak/",
   twitter: "https://twitter.com/ar_sahak",
   insta: "https://www.instagram.com/ar_sahak_/",
